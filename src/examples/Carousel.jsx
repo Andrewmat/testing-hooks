724d504e298b1552562be116a7f6046fd4801154
Andrewmat <andre.m.santos1@gmail.com>
2019-01-26 22:59:49 -0200

    * Adds Card

--- src/examples/Carousel.jsx
+++ src/examples/Carousel.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Carousel from '../components/Carousel';
+import Card from '../components/Card';
 import Async from '../components/Async';
 import useToggle from '../hooks/useToggle';
 import { fetchPerson } from '../services/swService';
@@ -37,33 +38,9 @@ export default () => {
                 (data, error) => (
                   <div className="carousel-item">
                     {error ? (
-                      <>
-                        <span className="carousel-item__error">Error: {error.message}</span>
-                      </>
+                      <span className="carousel-item__error">Error: {error.message}</span>
                     ) : (
-                      <>
-                        <span className="carousel-item__title">{data.name}</span>
-                        <div className="carousel-item__data">
-                          <div>Eye color:</div>
-                          <div>{data.eye_color}</div>
-                        </div>
-                        <div className="carousel-item__data">
-                          <div>Skin color:</div>
-                          <div>{data.skin_color}</div>
-                        </div>
-                        <div className="carousel-item__data">
-                          <div>Hair color:</div>
-                          <div>{data.hair_color}</div>
-                        </div>
-                        <div className="carousel-item__data">
-                          <div>Height:</div>
-                          <div>{data.height}cm</div>
-                        </div>
-                        <div className="carousel-item__data">
-                          <div>Mass:</div>
-                          <div>{data.mass}kg</div>
-                        </div>
-                      </>
+                      <Card data={data} />
                     )}
                   </div>
                 )
