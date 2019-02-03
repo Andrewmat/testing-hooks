2854b1c564a4eefb66fbb5f7ea6239eac6a1c78a
Andrewmat <andre.m.santos1@gmail.com>
2019-02-03 11:18:33 -0200

    Updates

---
+++ precache-manifest.c6f3d19f0ea57ac325b0c411a07a9ad2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d1d03d5b078e55ed3d5",
+    "url": "/hooks/static/css/main.d2c7f7bb.chunk.css"
+  },
+  {
+    "revision": "7d1d03d5b078e55ed3d5",
+    "url": "/hooks/static/js/main.7d1d03d5.chunk.js"
+  },
+  {
+    "revision": "5b088e8232ee72b88df7",
+    "url": "/hooks/static/js/1.5b088e82.chunk.js"
+  },
+  {
+    "revision": "8c6c9afa896489816e41",
+    "url": "/hooks/static/js/runtime~main.8c6c9afa.js"
+  },
+  {
+    "revision": "f645ca56abc1b4e0eccafa58bad76d6f",
+    "url": "/hooks/index.html"
+  }
+];
