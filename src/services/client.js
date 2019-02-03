d14dd503fc18a4abedb5c94f82e3f3c439116145
Andrewmat <andre.m.santos1@gmail.com>
2019-02-03 12:08:38 -0200

    Updates

--- src/services/client.js
+++
@@ -1,7 +0,0 @@
-export async function fetchJson(url) {
-  const response = await fetch(url)
-  if (!response.ok) {
-    throw response
-  }
-  return response.json()
-}
--- src/services/swService.js
+++
@@ -1,5 +0,0 @@
-import { fetchJson } from './client'
-
-export async function getPerson(id) {
-  return fetchJson(`https://swapi.co/api/people/${id}/`)
-}
