b736621fd202bd26b59d26af83d1714f658fd677
Andrewmat <andre.m.santos1@gmail.com>
2019-02-03 23:45:30 -0200

    Github link

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import classnames from 'classnames'
 import CacheProvider from './components/CacheProvider'
 import CardIterator from './components/CardIterator'
 import useToggle from './hooks/useToggle'
+import github from './github.png'
 import './App.scss'
 
 const App = () => {
@@ -22,6 +23,9 @@ const App = () => {
       <CacheProvider>
         <CardIterator darkMode={darkMode} />
       </CacheProvider>
+      <div className="github-link">
+        Check it on <a href="https://github.com/Andrewmat/testing-hooks" target="_blank"><img src={github}/><span>Github</span></a>
+      </div>
     </div>
   )
 }
